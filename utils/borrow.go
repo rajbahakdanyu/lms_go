ae43957cd49cabdf0fb9d439deb09c97035e021a
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-22 21:22:00 +0545

    Fixed check condition for finding book

--- utils/borrow.go
+++ utils/borrow.go
@@ -37,21 +37,25 @@ func old_borrower(name string) {
 	r, _ := regexp.Compile("(^[0-9]*$)")
 
 	reader := bufio.NewReader(os.Stdin)
-	println("Enter Book name")
+	println("Enter Book Id")
 	text, _ := reader.ReadString('\n')
 	choice := strings.TrimSpace(text)
+	println()
 
 	if r.MatchString(choice) {
 		check_book := false
+		var x string
 
 		for _, j := range booklist {
-			if strings.Contains(j, choice) {
+			if strings.Split(j, ",")[0] == choice {
 				check_book = true
+				x = j
 			}
+
 		}
 
 		if check_book {
-			println("Book exists")
+			println("user has chosen " + x)
 		} else {
 			println("Book does not exist")
 		}
