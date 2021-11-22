916ace2fd862f51315a2dd675d91377b290cec87
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-22 14:49:42 +0545

    Added logic to take input from console

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Welcome to the Library of Alexandria")
+	fmt.Println("------------------------------------")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Select an action by pressing \n1. For Borrowing a Book \n2. For Returning a Book \n3. To Exit")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
 }
