b697457077815711fac3e46c85ba0c76849f6fcd
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-22 18:09:41 +0545

    Added logic for infinite loop

--- main.go
+++ main.go
@@ -4,14 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Welcome to the Library of Alexandria")
 	fmt.Println("------------------------------------")
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Select an action by pressing \n1. For Borrowing a Book \n2. For Returning a Book \n3. To Exit")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Select an action by pressing \n1. For Borrowing a Book \n2. For Returning a Book \n3. To Exit")
+		text, _ := reader.ReadString('\n')
+		choice := strings.TrimSpace(text)
+
+		switch choice {
+		case "1":
+			fmt.Println("one")
+		case "2":
+			fmt.Println("two")
+		case "3":
+			fmt.Println("Good Bye")
+		default:
+			fmt.Println("Please enter correct code.")
+		}
+
+		if choice == "3" {
+			break
+		}
+	}
 }
