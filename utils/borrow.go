43d5219279fdee34d6838f4544e26df71529e7a1
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-22 21:11:15 +0545

    Added logic to check if user enters valid book id

--- utils/borrow.go
+++ utils/borrow.go
@@ -2,16 +2,14 @@ package utils
 
 import (
 	"bufio"
-	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func Borrow() {
-	DisplayBooklist()
-
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter Borrower name")
+	println("Enter Borrower name")
 	text, _ := reader.ReadString('\n')
 	choice := strings.TrimSpace(text)
 
@@ -31,10 +29,35 @@ func Borrow() {
 }
 
 func old_borrower(name string) {
-	dat, err := os.ReadFile(fmt.Sprintf("members/%v.txt", name))
-	Check(err)
+	// dat, err := os.ReadFile(fmt.Sprintf("members/%v.txt", name))
+	// Check(err)
 
-	println(string(dat))
+	DisplayBooklist()
+	booklist := GetBooklist()
+	r, _ := regexp.Compile("(^[0-9]*$)")
+
+	reader := bufio.NewReader(os.Stdin)
+	println("Enter Book name")
+	text, _ := reader.ReadString('\n')
+	choice := strings.TrimSpace(text)
+
+	if r.MatchString(choice) {
+		check_book := false
+
+		for _, j := range booklist {
+			if strings.Contains(j, choice) {
+				check_book = true
+			}
+		}
+
+		if check_book {
+			println("Book exists")
+		} else {
+			println("Book does not exist")
+		}
+	} else {
+		println("Book Id should be a number\n")
+	}
 }
 
 func new_borrower(name string) {
