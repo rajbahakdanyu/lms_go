9a19507edc4d82a8b6857d20ecb212ba0eb39a79
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-11-24 13:27:45 +0545

    Added return function logic

--- utils/return.go
+++ utils/return.go
@@ -18,16 +18,47 @@ func Return() {
 		Check(err)
 
 		if strings.Contains(string(dat), choice) {
+			check_book := false
 			return_list := GetReturnlist(choice)
 
 			println("\nBook name\t\tPrice($)\tBorrow date\t\tDeadline\t\tStatus")
 			for _, x := range return_list {
 				if len(x) > 0 {
+					check_book = true
 					temp := strings.Split(x, ",")
 					if y := strings.TrimSpace(temp[4]); y == "not returned" {
 						fmt.Printf("%v\t\t%v\t\t%v\t\t%v\t\t%v", temp[0], temp[1], temp[2], temp[3], temp[4])
 					}
 				}
+			}
+			println("")
+
+			if check_book {
+				reader := bufio.NewReader(os.Stdin)
+				println("Enter Book name")
+				text, _ := reader.ReadString('\n')
+				book := strings.TrimSpace(text)
+				check_choice := true
+				var return_list []string
+
+				for _, x := range return_list {
+					if len(x) > 0 {
+						temp := strings.Split(x, ",")
+
+						if y := strings.TrimSpace(temp[4]); y == "not returned" && temp[0] == book {
+							check_choice = false
+							return_list = temp
+						}
+					}
+				}
+
+				if check_choice {
+					fmt.Printf("%v has not borrowed %v", choice, book)
+				} else {
+					book_return(choice, book, return_list)
+				}
+			} else {
+				fmt.Printf("%v has not borrowed any books\n", choice)
 			}
 		} else {
 			fmt.Printf("%v has not borrowed any books\n", choice)
@@ -37,3 +68,5 @@ func Return() {
 		Return()
 	}
 }
+
+func book_return(name string, book string, return_list []string) {}
